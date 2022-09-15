d26e1aacc7bbabf1d43c995f12a92eb09d48b46f
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-15 07:38:56 -0400

    Connect Slider to the store

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,24 +1,24 @@
-import { Component, createSignal } from 'solid-js';
+import { Component } from 'solid-js';
+import store, { setStoreLength } from '../../stores/password';
 
 import CustomSlider from './CustomSlider';
 
 import styles from './Form.module.css';
 
 const Form: Component = () => {
-  const [length, setLength] = createSignal(0);
   return (
     <form class={styles.form}>
       <div class={styles.charlength}>
         <label for="char-length">Character Length</label>
-        <span>{length()}</span>
+        <span>{store.length}</span>
         <CustomSlider
           id="char-length"
           name="char-length"
           min={0}
           max={20}
           step={1}
-          value={length()}
-          onInput={(e) => setLength(e.currentTarget.valueAsNumber)}
+          value={store.length}
+          onInput={(e) => setStoreLength(e.currentTarget.valueAsNumber)}
         />
       </div>
     </form>
