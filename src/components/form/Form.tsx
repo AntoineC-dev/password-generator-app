d03e587dc8acd6349acbe707eaa9e777e16a565f
Antoine Cheminat <ac.dev@hotmail.com>
2022-09-16 01:17:49 -0400

    Create generateBtn tooltip

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,4 +1,4 @@
-import { Component, createMemo, For } from 'solid-js';
+import { Component, createEffect, createMemo, For } from 'solid-js';
 import store, { setStoreLength, setStorePassword, setStoreRule } from '../../stores/password';
 import { rules } from '../../types';
 
@@ -9,7 +9,7 @@ import StrengthMeter from './StrengthMeter';
 import styles from './Form.module.css';
 
 const Form: Component = () => {
-  const generateDisabled = createMemo(() => store.length === 0 || store.length < store.password.strength);
+  createEffect(() => console.log(store.errorMsg));
   const onSubmit = (e: Event) => {
     e.preventDefault();
     setStorePassword();
@@ -42,7 +42,7 @@ const Form: Component = () => {
         </For>
       </div>
       <StrengthMeter />
-      <button class={styles.generate} aria-disabled={generateDisabled()}>
+      <button class={styles.generate} data-tooltip={store.errorMsg} aria-disabled={!!store.errorMsg}>
         <span>Generate</span>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12">
           <path fill="currentColor" d="m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z" />
